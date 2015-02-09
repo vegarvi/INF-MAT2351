87c3b97fed5772c49c248ea3918503f284d60096
vegarvi <vegarvi@student.matnat.uio.no>
2015-02-09 15:22:38 +0100

    first uploads

--- week3/p242_class.py
+++ week3/p242_class.py
@@ -1,5 +1,7 @@
 from matplotlib.pylab import *
 class solver:
+	# Class to solve an ODE on the form
+	# u' = f(u,t), works for the explicit schemes FE, Heun, RK4
 	def __init__(self,f,u0,T,N):
 		self.f = f
 		self.T = T
@@ -12,7 +14,7 @@ class solver:
 	def solve(self):
 		self.u[0] = self.u0	
 		for n in range(self.N):
-			self.u[n+1] = self.step(self.u[n],self.t[n])
+			self.u[n+1] = self.step(self.u[n],self.t[n])    # depends on method
 	
 		return self.u,self.t
 
@@ -43,40 +45,34 @@ class RK4(solver):
 		u = u_ + (self.dt/6.0)*(F1+2*F2+2*F3+F4)
 		return u
 
+# ex n)
 
-T = 2.0
-
-eps = 0.5
+T = 1.1
+eps = 0.25
 f = lambda u,t: 2*(1-t)*u/eps**2
+
 u0 = exp(-1/eps**2)
-err = 10
 u_ex = lambda t: u0*exp((2-t)*t/eps**2)
-'''
 
-f = lambda u,t: u*(1-u)
-
-
-
-u0 = 10
-solver = RK4(f,u0,T,100)
-u,t = solver.solve()
-u_ex = exp(t)*(exp(t)-0.9)**-1
-plot(t,u)
-show()
-'''
-
-i = 1
 import time
-while err > 1E-5:
-	N = 10*2**i
-	solver = Heun(f,u0,T,N)
-	t1 = time.time()
-	solver.solve()
-	time_used = time.time()-t1
-	err = solver.error(u_ex)
-	i *= 2
-	print err, time_used
-	
-	
-	#plot(t,u,t,exp(-((1-t)/eps)**2))
-	#show()
+for method in range(3):
+	i = 1
+	err = 10
+	while err > 1E-4:    # with FE, it takes a long time to reach 10**-5
+		N = 100*2**i
+		if method == 0:
+			solver = FE(f,u0,T,N)
+		elif method == 1:
+			solver = Heun(f,u0,T,N)
+		else: solve = RK4(f,u0,T,N)
+		t1 = time.time()
+		solver.solve()
+		time_used = time.time()-t1
+		err = solver.error(u_ex)
+
+		i += 1
+	print err, time_used,N
+
+# conclusion for this problem: FE is simple, but slow. Heun is fast but not as accurate as RK4.
+# RK4 is accurate but needs more time per iteration. (More operations at each step)
+
